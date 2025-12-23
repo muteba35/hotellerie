a93e20a26e45b3e392a483227076375aed225f2f
muteba <juniormuteba10@gmail.com>
2025-12-23 16:43:49 +0100

    Add Netlif

--- backend/index.js
+++ backend/index.js
@@ -5,26 +5,54 @@ require("dotenv").config();
 
 const app = express();
 
-// middlewares
-app.use(cors());
+/*CORS CONFIGURATION */
+const allowedOrigins = [
+  "https://hotellerie.onrender.com",
+  "http://localhost:3000",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // autorise mobile / navigateur sans origin (ex: apps)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+// important pour preflight (mobile)
+app.options("*", cors());
+
+/* MIDDLEWARES */
 app.use(express.json());
 
-// test route
+/* TEST ROUTE*/
 app.get("/", (req, res) => {
   res.send("Backend Hotellerie OK");
 });
 
-// routes
+/* ROUTES*/
 const registerRoute = require("./src/routes/auth.register");
 app.use("/api/auth", registerRoute);
 
-// mongodb
+/* MONGODB*/
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connecté avec succès"))
   .catch((err) => console.error("Erreur MongoDB :", err));
 
+/* SERVER */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Serveur lancé sur http://localhost:" + PORT);
+  console.log("Serveur lancé sur http://localhost:" + PORT);
 });
