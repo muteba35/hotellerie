4e23509e63da9a79f8c3aba3de50fe46e8fec119
muteba <juniormuteba10@gmail.com>
2026-01-04 12:59:47 +0100

    Add Netlif

--- backend/src/user_mail/sendEmail.js
+++ backend/src/user_mail/sendEmail.js
@@ -2,17 +2,21 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,          
+    secure: false,      
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
 
-  
-  await transporter.verify(); 
-  console.log("Connexion Gmail OK");
-
+  // Test connexion SMTP
+  await transporter.verify();
+  console.log("SMTP Gmail connecté");
 
   await transporter.sendMail({
     from: `"Hotellerie" <${process.env.EMAIL_USER}>`,
